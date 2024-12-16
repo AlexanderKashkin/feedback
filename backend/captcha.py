3e5913732b2d057ccb43e98b550cebe7c42425f2
Alexander <a.kashkin@sigma.ru>
2024-12-16 21:43:00 +0300

    fix captcha, move secret in config

--- backend/captcha.py
+++ backend/captcha.py
@@ -4,12 +4,13 @@ from fastapi import HTTPException, status
 from backend.routes import logger
 from config.load_conf import config
 
+
 class CaptchaV3:
 
     def __init__(self, token: str):
         self.token = token
+        self.cfg = config.captcha
 
-    __secret = 'gref_not_gay'
     result = None
     timestamp = None
     hostname = None
@@ -19,16 +20,14 @@ class CaptchaV3:
     def token_site_verify(self):
         url = 'https://www.google.com/recaptcha/api/siteverify'
         logger.info('start token site verify')
-        resp = requests.post(url=url, json={'secret': self.__secret, 'response': self.token})
+        resp = requests.post(url=url, headers={'Content-Type' : 'application/x-www-form-urlencoded'}, data={'secret': self.cfg.secret, 'response': self.token})
         logger.info('end token site verify')
         response = resp.json()
         self.result = response.get('success', None)
         self.timestamp = response.get('timestamp', None)  # not required
         self.hostname = response.get('hostname', None)  # not required
         self.errors = response.get('error-codes', None)
-        if config.debug:
-            return
-        if resp.status_code != status.HTTP_200_OK or not self.result:  # status_code not 200 or success = False
+        if resp.status_code != status.HTTP_200_OK or self.result is False:  # status_code not 200 or success = False
             logger.error(
                 f'token site verify failed {self.token, self.result, self.timestamp, self.hostname, self.errors}')
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=self.errors)
